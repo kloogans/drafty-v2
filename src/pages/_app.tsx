7a48ec07b8a8a03a4da336190deae2a56c68902e
jamesfromonline <jtwebco@gmail.com>
2022-08-03 18:11:25 -0400

    adds cookie consent banner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,8 @@ import { AnalyticsProvider } from "use-analytics"
 import { analytics } from "src/lib/analytics"
 import { useEffect } from "react"
 import PaperTexture from "src/components/PaperTexture"
+import CookieConsent from "react-cookie-consent"
+import Link from "next/link"
 const TAG_MANAGER_CONFIG = {
   gtmId: "GTM-N27253L"
 }
@@ -39,6 +41,26 @@ function Drafty({ Component, pageProps }: AppProps) {
             pauseOnHover
             closeButton={false}
           />
+          <CookieConsent
+            location="bottom"
+            buttonText="Got it!"
+            cookieName="drafty-cookie-consent"
+            style={{ background: "#18181b" }}
+            expires={150}
+            buttonClasses="!bg-amber-400 hover:!bg-amber-500 focus:!bg-amber-500 !font-bold !text-sm !text-indigo-800 !rounded-2xl !px-4 !py-2"
+            contentClasses="!text-sm !text-white"
+          >
+            Drafty uses cookies to handle some of its necessary internal
+            workings.{" "}
+            <Link href="/cookies">
+              <a
+                title="Cookie Policy"
+                className="text-amber-400 hover:text-amber-500 focus:text-amber-500"
+              >
+                Learn more
+              </a>
+            </Link>
+          </CookieConsent>
         </AnalyticsProvider>
       </GlobalStateProvider>
     </SessionProvider>
